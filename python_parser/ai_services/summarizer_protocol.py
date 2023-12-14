04cf1bf70c24fc52a6a9058d84fc8717c3834de8
Evan <evanmschultz@gmail.com>
2023-12-14 14:34:07 -0700

    Moves `Summarizer` protocol class to its own module

---
+++ python_parser/ai_services/summarizer_protocol.py
@@ -0,0 +1,65 @@
+from typing import Protocol
+
+import ai_services.summarizer_configs as configs
+
+
+class Summarizer(Protocol):
+    def summarize_code(
+        self,
+        code: str,
+        *,
+        builder_id: str,
+        children_summaries: str | None,
+        dependency_summaries: str | None,
+        import_details: str | None,
+        configs: configs.SummaryCompletionConfigs = configs.SummaryCompletionConfigs(),
+    ) -> str:
+        """
+        Summarizes the provided code snippet using the OpenAI API.
+
+        Args:
+            code (str): The code snippet to summarize.
+            configs (SummaryCompletionConfigs, optional): Configuration settings for the summarization.
+                Defaults to SummaryCompletionConfigs().
+
+        Returns:
+            str: The summary of the provided code snippet.
+
+        Examples:
+            >>> client = OpenAI()
+            >>> summarizer = Summarizer(client=client)
+            >>> code_example = "print('Hello, world')"
+            >>> summary = summarizer.summarize_code(code_example)
+            >>> print(summary)
+        """
+        ...
+
+    def test_summarize_code(
+        self,
+        code: str,
+        *,
+        builder_id: str,
+        children_summaries: str | None,
+        dependency_summaries: str | None,
+        import_details: str | None,
+        configs: configs.SummaryCompletionConfigs = configs.SummaryCompletionConfigs(),
+    ) -> str:
+        """
+        Summarizes the provided code snippet using the OpenAI API.
+
+        Args:
+            code (str): The code snippet to summarize (pass in dummy string).
+            configs (SummaryCompletionConfigs, optional): Configuration settings for the summarization.
+                Defaults to SummaryCompletionConfigs().
+
+        Returns:
+            str: The summary of the provided code snippet.
+
+        Examples:
+            >>> client = OpenAI()
+            >>> summarizer = Summarizer(client=client)
+            >>> code_example = "print('Hello, world')"
+            >>> summary = summarizer.summarize_code(code_example)
+            >>> print(summary)
+        """
+        ...
