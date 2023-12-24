46f755c4f6e9696ab91ff32a9e7af1a17e61c646
Evan <evanmschultz@gmail.com>
2023-12-23 17:44:21 -0700

    Uncomments code as reminder that these should hold types when import model logic is updated to track down the code block that defined it

--- postcode/types/chroma.py
+++ postcode/types/chroma.py
@@ -1,26 +1,26 @@
-# """
-# ChromaDB Types
-# --------------
+"""
+ChromaDB Types
+--------------
 
-# This module contains types defined by the chromadb third-party library.
-# These types are used for easy implementation in the postcode project and
-# provide convenience for users of the postcode library.
-# """
+This module contains types defined by the chromadb third-party library.
+These types are used for easy implementation in the postcode project and
+provide convenience for users of the postcode library.
+"""
 
-# from chromadb.config import DEFAULT_DATABASE, DEFAULT_TENANT, Settings
-# from chromadb.api import ClientAPI
-# from chromadb.api.types import (
-#     DataLoader,
-#     CollectionMetadata,
-#     GetResult,
-#     QueryResult,
-#     Where,
-#     WhereDocument,
-#     Include,
-#     URIs,
-#     Loadable,
-#     Metadata,
-#     Embedding,
-# )
-# from chromadb import Collection
-# from chromadb import EmbeddingFunction
+from chromadb.config import DEFAULT_DATABASE, DEFAULT_TENANT, Settings
+from chromadb.api import ClientAPI
+from chromadb.api.types import (
+    DataLoader,
+    CollectionMetadata,
+    GetResult,
+    QueryResult,
+    Where,
+    WhereDocument,
+    Include,
+    URIs,
+    Loadable,
+    Metadata,
+    Embedding,
+)
+from chromadb import Collection
+from chromadb import EmbeddingFunction
--- postcode/types/openai.py
+++ postcode/types/openai.py
@@ -1,8 +1,8 @@
-# from openai.types.chat.chat_completion_system_message_param import (
-#     ChatCompletionSystemMessageParam,
-# )
-# from openai.types.chat.chat_completion_user_message_param import (
-#     ChatCompletionUserMessageParam,
-# )
-# from openai.types.chat.chat_completion_message_param import ChatCompletionMessageParam
-# from openai.types.chat.chat_completion import ChatCompletion
+from openai.types.chat.chat_completion_system_message_param import (
+    ChatCompletionSystemMessageParam,
+)
+from openai.types.chat.chat_completion_user_message_param import (
+    ChatCompletionUserMessageParam,
+)
+from openai.types.chat.chat_completion_message_param import ChatCompletionMessageParam
+from openai.types.chat.chat_completion import ChatCompletion
