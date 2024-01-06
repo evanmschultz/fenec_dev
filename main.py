cdccb853c7b1a5f37e9210c848c28f430c768c93
Evan <evanmschultz@gmail.com>
2024-01-06 16:09:43 -0700

    Creates simple CLI app

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-from postcode import main
+from postcode import simple_chat, setup_chroma
 
-main("postcode")
+
+def main():
+    chroma_collection_manager = setup_chroma()
+    simple_chat(chroma_collection_manager)
+
+
+if __name__ == "__main__":
+    main()
--- postcode/__init__.py
+++ postcode/__init__.py
@@ -1,1 +1,2 @@
-from postcode.app import main
+from postcode.app import simple_chat
+from postcode.databases.chroma.chroma_setup import setup_chroma
